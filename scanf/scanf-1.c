bca8ad0833dcc199d6339ac51089491f61274555
Alik Agarwala <alikagarwala@outlook.com>
2023-07-28 21:41:28 +0530

    Create scanf/scanf-1.c

---
+++ scanf/scanf-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    printf("Enter a value: ");
+
+    scanf("%d", &x); // taking input for variable x
+
+    printf("My value is %d", x);
+
+    return 0;
+}
