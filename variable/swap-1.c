f7c58dffae942e8dd28b1cffca8156675af8ae6e
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 16:11:50 +0530

    create variable/swap-1.c

---
+++ variable/swap-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5, b=6;
+
+    //swap
+    int  temp = a;
+    a=b;
+    b=temp;
+
+    printf("a = %d\n", a);
+    printf("a = %d\n", b);
+
+    return 0;
+}
