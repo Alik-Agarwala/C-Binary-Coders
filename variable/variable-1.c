0745f475ef0710ee74306fdf394a170afe1a6797
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 16:09:47 +0530

    modify variable/variable-1.c

--- variable/variable-1.c
+++ variable/variable-1.c
@@ -7,13 +7,13 @@ int main()
     char my_name;
 
     int x;
-    x=5;
+    x = 5;
 
     int z = 6;
 
-    float a,b,c, mango, banana;
+    float a, b, c, mango, banana;
 
     float m, n = 7.6, p;
-    
+
     return 0;
 }
