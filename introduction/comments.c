895e96ded5a7a63719593f165a226a857b3a6045
Alik Agarwala <alikagarwala@outlook.com>
2023-07-28 21:18:29 +0530

    Create introduction/comments.c

---
+++ introduction/comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int length; // this is variable declaration (single line comment)
+
+    /*
+        this is a multiline comment
+    */
+
+    printf("Hello coders!");
+
+    return 0;
+}
