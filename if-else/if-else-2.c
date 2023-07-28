000ecc11015cc1817414d14bd29eeb62ed620e41
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 16:38:08 +0530

    add if-else/if-else-2.c

---
+++ if-else/if-else-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2, b = 10;
+
+    if (a > b)
+        printf("a is larger");
+
+    printf("Hello"); // this is not inside if block as braces are not present
+
+    return 0;
+}
