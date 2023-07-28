37411cc14c61d130d5c0833229b48338146029b3
Avik Agarwala <avikagarwala@outlook.com>
2023-07-28 16:06:40 +0530

    create variable/variable-1.c

---
+++ variable/variable-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int length;
+    float width;
+    char my_name;
+
+    int x;
+    x=5;
+
+    int z = 6;
+
+    float a,b,c, mango, banana;
+
+    float m, n = 7.6, p;
+    
+    return 0;
+}
