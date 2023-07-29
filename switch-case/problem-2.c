d796887f4447449aa6d6b3d326a94a3c07ee861e
Alik Agarwala <alikagarwala@outlook.com>
2023-07-29 14:57:03 +0530

    Add switch-case/problem-2.c

---
+++ switch-case/problem-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    int rem = ((n % 3) && (n % 5));
+
+    switch (rem)
+    {
+    case 0:
+        printf("Success\n");
+        break;
+
+    default:
+        printf("OOPS\n");
+    }
+
+    return 0;
+}
