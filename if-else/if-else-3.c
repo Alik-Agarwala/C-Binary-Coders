bccbc31a839055100fed34bb4c9df6b2c9a6d4a0
Alik Agarwala <alikagarwala@outlook.com>
2023-08-14 20:40:11 +0530

    add if-else-3.c

---
+++ if-else/if-else-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2, b = 10;
+
+    if (a > b) // false
+    {
+        printf("A is larger\n");
+        printf("Hi\n");
+    }
+
+    printf("Bye\n");
+
+    return 0;
+}
