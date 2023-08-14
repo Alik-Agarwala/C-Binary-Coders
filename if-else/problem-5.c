eb54f203186ab0e4e97c9fbb8653bb7e8e5df0e5
Alik Agarwala <alikagarwala@outlook.com>
2023-08-14 21:20:25 +0530

    add problem-5.c

---
+++ if-else/problem-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks = 95;
+
+    if (!(marks > 90)) // using logical NOT
+        printf("Bravo!");
+    else
+        printf("Get lost!");
+
+    return 0;
+}
